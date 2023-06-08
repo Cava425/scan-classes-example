cd05449c1016587f945b59b6e1f4a17f635c43a9
xuyongshi <heliocc@163.com>
2023-06-08 22:11:13 +0800

    init

---
+++ src/main/java/com/example/scan/ScanClassesExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.scan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScanClassesExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScanClassesExampleApplication.class, args);
+    }
+
+}
